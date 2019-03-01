cfe4e3cb876b209c80c8da1e199a2091d9584513
sivakumar <sivainvitesyou@gmail.com>
2019-03-01 11:08:48 +0530

    Initial Commit

---
+++ BlueToothSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlueToothSample
+//
+//  Created by TechMahindra on 01/03/19.
+//  Copyright © 2019 Sivakumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
